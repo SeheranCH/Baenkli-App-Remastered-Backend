a3a1d9e50c8d69529028de732424dd06a14c997e
Sabrina Westhauser <sabrina.westhauser@edu.tbz.ch>
2021-01-25 00:16:01 +0100

    new files

--- build/generated/sources/annotationProcessor/java/main/ch/tbz/m326/BaenkliApplication/domainModells/rating/mapper/RatingMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/ch/tbz/m326/BaenkliApplication/domainModells/rating/mapper/RatingMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-01-18T16:16:54+0100",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.1 (Oracle Corporation)"
+    date = "2021-01-24T23:49:37+0100",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
 )
 @Component
 public class RatingMapperImpl implements RatingMapper {
